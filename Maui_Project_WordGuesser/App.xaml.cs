f1a0bd54392d48472d4435eca3b41ada6b390f2e
Donghwi_Park <gptnc5100@gmail.com>
2024-10-07 11:56:49 +0200

    Add project files.

---
+++ Maui_Project_WordGuesser/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Maui_Project_WordGuesser
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Maui_Project_WordGuesser/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Maui_Project_WordGuesser
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
