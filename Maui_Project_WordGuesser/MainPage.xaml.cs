647e26fac1e5f2014e4036acc8edfc0d645e4944
Donghwi_Park <gptnc5100@gmail.com>
2024-10-08 10:05:43 +0200

    made the words to guess.

--- Maui_Project_WordGuesser/MainPage.xaml.cs
+++ Maui_Project_WordGuesser/MainPage.xaml.cs
@@ -1,13 +1,59 @@
-﻿namespace Maui_Project_WordGuesser
+﻿using System.ComponentModel;
+using System.Diagnostics;
+
+namespace Maui_Project_WordGuesser
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
+        #region Fields
+        List<string> words = new List<string>()
+     {
+        "python",
+        "html",
+        "css",
+        "javascript",
+        "java",
+        "go",
+        "dart",
+        "flutter",
+        "php",
+        "swift",
+        "cobol",
+        "typescript",
+        "kotlin",
+        "perl",
+        "mysql",
+        "ruby",
+        "delphi",
+        "haskell",
+        "c"
+        "maui",
+        "csharp",
+        "mongodb",
+        "sql",
+        "xaml",
+        "word",
+        "excel",
+        "powerpoint",
+        "code",
+        "hotreload",
+        "snippets"
+     };
+        string answer = "";
+        #endregion
+
         public MainPage()
         {
             InitializeComponent();
+            PickWord();
         }
 
-      
+        #region Game Engine
+        private void PickWord()
+        {
+            answer = words[new Random().Next(0, words.Count)];
+            Debug.WriteLine(answer);
+        }
+        #endregion
     }
-
 }
